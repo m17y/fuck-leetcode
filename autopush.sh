cb0404c863c07b3c310a86a17a5480d1e458ee92
xk <xk@MacBook-Pro.local>
2020-06-26 18:10:04 +0800

    . 2020-06-26 18:10:04

--- autopush.sh
+++ autopush.sh
@@ -1,13 +1,14 @@
+#! /bin/sh 
+source /etc/profile 
 the_day=$(date "+%Y-%m-%d %H:%M:%S")
 status=`git status`
 diff=`git diff`
 echo ${the_day}
-echo ${#status} #字符串长度
+echo ${#status} #获取字符串长度
 # 判断是否包含
 if [[ $status =~ '干净的工作区' ]];then 
     echo "最新没有 新的文件更改"  
 else
-
     echo "有文件有修改，开始提交文件"
     git add -A
     git commit -am ". ${the_day}"

0cb6a416865738add13f544c1b61817fb2d4c49a
xk <xk@MacBook-Pro.local>
2020-06-26 17:48:57 +0800

    . 2020-06-26 17:48:57

--- 189.旋转数组.py
+++ 189.旋转数组.py
@@ -12,6 +12,15 @@ class Solution(object):
         :type k: int
         :rtype: None Do not return anything, modify nums in-place instead.
         """
+        # 可以用额外空间保存数组
+        
+        # 思路一：插入
+
+        # 思路二：拼接
+
+        # 思路三：三个翻转 ， 整体翻转， 前k翻转，后k翻转
+
+        # 思路四：模拟过程
         n = len(nums)
         k %= n
         nums[:] = nums[::-1]

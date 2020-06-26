0d9f170798abeb117429653a904c140ba6ddd24a
xk <xk@MacBook-Pro.local>
2020-06-26 15:32:00 +0800

    .

---
+++ 53.最大子序和.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode.cn id=53 lang=python
+#
+# [53] 最大子序和
+#
+
+# @lc code=start
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+# @lc code=end
+

82917b5a6acd895ab91faf62750a8e70ed9c1677
xk <xk>
2020-06-26 21:28:00 +0800

    . 2020-06-26 21:28:00

--- autopush.sh
+++ autopush.sh
@@ -5,15 +5,17 @@ the_day=$(date "+%Y-%m-%d %H:%M:%S")
 status=`git status`
 diff=`git diff`
 echo ${the_day}
-echo ${#status} #获取字符串长度
+#获取字符串长度
+echo ${#status}
 # 判断是否包含
+
 if [[ $status =~ '干净的工作区' ]];then 
     echo "最新没有 新的文件更改"  
 else
     echo "有文件有修改，开始提交文件"
     git add -A
     git commit -am ". ${the_day}"
-    git push
+    git push -u origin master
     echo ${push}
     echo "提交文件结束"
 fi    

8a98987a3cbee90e9c6820cc8f093f0e65337c11
xk <xk>
2020-06-27 21:45:01 +0800

    . 2020-06-27 21:45:00

---
+++ 283.移动零.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode.cn id=283 lang=python
+#
+# [283] 移动零
+#
+
+# @lc code=start
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        
+# @lc code=end
+

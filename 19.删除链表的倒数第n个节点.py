001c8834a0f9fbd7c802baf3497efa8897775baa
xk <xk>
2020-06-27 22:10:00 +0800

    . 2020-06-27 22:10:00

---
+++ 19.删除链表的倒数第n个节点.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode.cn id=19 lang=python
+#
+# [19] 删除链表的倒数第N个节点
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+# @lc code=end
+

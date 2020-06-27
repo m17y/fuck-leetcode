a54bc55326f6e33e2112b573e2605646334764eb
xk <xk>
2020-06-27 21:25:00 +0800

    . 2020-06-27 21:25:00

--- 66.加一.py
+++ 66.加一.py
@@ -17,9 +17,16 @@ class Solution(object):
         l = len(digits)
         ls = range(l)[::-1]
         next = 1
+        print ls
         for i in ls:
-            if i+next==10:
+            if digits[i]+next>=10:
                 next = 1
-                
+                digits[i] = (digits[i]+next)%10
+            else:
+                digits[i] = digits[i]+next
+                next = 0
+        if next == 1:
+            digits.insert(0,1)
+        return digits
 # @lc code=end
 

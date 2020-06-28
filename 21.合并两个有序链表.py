d2c335efd110612d30d76f2f3030126ef7159ac8
xk <xk>
2020-06-28 22:20:00 +0800

    . 2020-06-28 22:20:00

--- 21.合并两个有序链表.py
+++ 21.合并两个有序链表.py
@@ -23,20 +23,17 @@ class Solution(object):
             tmp1 = l1.next
             tmp22 = l2
             while l2:
-                
                 tmp2 = l2.next
                 if l1.val<=l2.val:
-                    l1.next = tmp2
+                    l1.next = None
                     l2.next = l1
-                    l2head.next = l2
-                    l2head.next
-                else:
-                    l2head.next = l2
+                l2head.next = l2
+                print l2head
                 l2 = tmp2
             if not l2:
                 l2head.next = l1
             l1 = tmp1
-        return l2
+        return l2head
    
 # @lc code=end
 

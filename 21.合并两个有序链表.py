501984fb93f0916bb41ebd16a2b4a4a84c1d9585
xk <xk>
2020-06-28 22:25:00 +0800

    . 2020-06-28 22:25:00

--- 21.合并两个有序链表.py
+++ 21.合并两个有序链表.py
@@ -17,23 +17,25 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        pre =None
-        l2head = ListNode(None)
-        while l1:
-            tmp1 = l1.next
-            tmp22 = l2
-            while l2:
-                tmp2 = l2.next
-                if l1.val<=l2.val:
-                    l1.next = None
-                    l2.next = l1
-                l2head.next = l2
-                print l2head
-                l2 = tmp2
-            if not l2:
-                l2head.next = l1
-            l1 = tmp1
-        return l2head
+        # prehead = ListNode(-1)
+        # prev = prehead
+        # while l1:
+        #     tmp1 = l1.next
+        #     tmp22 = l2
+        #     while l2:
+        #         tmp2 = l2.next
+        #         if l1.val<=l2.val:
+        #             l1.next = None
+        #             l2.next = l1
+        #             prev.next = l2
+        #             prev = prev.next
+        #         l2 = tmp2
+        #     if not l2:
+        #         l1.next = None
+        #         prev.next = l1
+        #         prev = prev.next
+        #     l1 = tmp1
+        # return prehead.next
    
 # @lc code=end
 

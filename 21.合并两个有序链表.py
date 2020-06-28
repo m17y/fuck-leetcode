12e4ee937bb7b590e0820f9d2bebc57bf6cf2a46
xk <xk>
2020-06-28 21:45:01 +0800

    . 2020-06-28 21:45:01

--- 21.合并两个有序链表.py
+++ 21.合并两个有序链表.py
@@ -17,10 +17,15 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        while l1:
-            while l2:
-               if l1.val>l2.val:
-                    
+        end1 = None
+        tmp1 = l1
+        while tmp1:
+            tmp1 = tmp1.next
+        tmp1.next = l2
+
+        while 
+            
+
 
         
 # @lc code=end

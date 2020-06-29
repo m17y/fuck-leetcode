f63907b75e0993da0088d7aebe03272f60563b32
xk <xk>
2020-06-29 21:00:00 +0800

    . 2020-06-29 21:00:00

---
+++ 70.爬楼梯.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode.cn id=70 lang=python
+#
+# [70] 爬楼梯
+#
+
+# @lc code=start
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        
+# @lc code=end
+

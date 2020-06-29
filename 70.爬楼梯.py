35a3a73754ddbb3abe4d052ddb82dd4fe8ed6de5
xk <xk>
2020-06-29 21:20:00 +0800

    . 2020-06-29 21:20:00

--- 70.爬楼梯.py
+++ 70.爬楼梯.py
@@ -16,10 +16,11 @@ class Solution(object):
         # if n==1:return 1
         # if n==2:return 2
         # return self.climbStairs(n-1)+self.climbStairs(n-2)
+        if n==1:return 1
+        if n==2:return 2
         dp =[1,2]
         for i in range(2,n):
-            print i,dp[i-1]+dp[i-2]
             dp[i] = dp[i-1]+dp[i-2]
-        return dp[-1]
+        return dp
 # @lc code=end
 

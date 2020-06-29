91e2ad2cb45b05f2c583484c42400ba3980d4b8b
xk <xk>
2020-06-29 21:15:00 +0800

    . 2020-06-29 21:15:00

--- 70.爬楼梯.py
+++ 70.爬楼梯.py
@@ -12,9 +12,14 @@ class Solution(object):
         :rtype: int
         """
         # f(x)=f(x−1)+f(x−2)
-        if n==1:return 1
-        if n==2:return 2
-        return climbStairs(n-1)+climbStairs(n-2)
-
+        # 递归 时间复杂度会很高
+        # if n==1:return 1
+        # if n==2:return 2
+        # return self.climbStairs(n-1)+self.climbStairs(n-2)
+        dp =[1,2]
+        for i in range(2,n):
+            print i,dp[i-1]+dp[i-2]
+            dp[i] = dp[i-1]+dp[i-2]
+        return dp[-1]
 # @lc code=end
 

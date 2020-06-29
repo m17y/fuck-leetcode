67bdbb07a3f02d722c156fd5812c88c30779f3b5
xk <xk>
2020-06-29 20:45:00 +0800

    . 2020-06-29 20:45:00

---
+++ 234.回文链表.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode.cn id=234 lang=python
+#
+# [234] 回文链表
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+# @lc code=end
+

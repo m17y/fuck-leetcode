01704acc3e485802223cf42c1a9aa31f980d3b8e
xk <xk>
2020-07-02 21:55:01 +0800

    . 2020-07-02 21:55:01

---
+++ 46.全排列.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode.cn id=46 lang=python
+#
+# [46] 全排列
+#
+
+# @lc code=start
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        stock = 
+        for 
+# @lc code=end
+

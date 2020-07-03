aae5384eaf704241178877b8f6d96f91d7847035
xk <xk>
2020-07-03 23:24:28 +0800

    . 2020-07-03 23:24:28

---
+++ 242.有效的字母异位词.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode.cn id=242 lang=python
+#
+# [242] 有效的字母异位词
+#
+
+# @lc code=start
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+# @lc code=end
+

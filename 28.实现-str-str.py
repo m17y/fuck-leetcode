9e09abc01eb2aded3131126f1e8fa6a0503b88c2
xk <xk@xk.com>
2020-07-07 20:10:00 +0800

    . 2020-07-07 20:10:00

---
+++ 28.实现-str-str.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode.cn id=28 lang=python
+#
+# [28] 实现 strStr()
+#
+
+# @lc code=start
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+# @lc code=end
+

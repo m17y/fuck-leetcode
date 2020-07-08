f1e14b9891e8839a3bcac49233cce533d6254aa1
xk <xk@xk.com>
2020-07-08 22:05:00 +0800

    . 2020-07-08 22:05:00

---
+++ 40.组合总和-ii.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode.cn id=40 lang=python
+#
+# [40] 组合总和 II
+#
+
+# @lc code=start
+class Solution(object):
+    def combinationSum2(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+# @lc code=end
+

12152429d03ab1d111f67e63f96bf35dac6e06bb
xk <xk@xk.com>
2020-07-08 22:10:01 +0800

    . 2020-07-08 22:10:00

--- 40.组合总和-ii.py
+++ 40.组合总和-ii.py
@@ -12,5 +12,30 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
+        def dfs(begin, path, residue):
+            if residue == 0:
+                res.append(path[:])
+                return
+
+            for index in range(begin, size):
+                if candidates[index] > residue:
+                    break
+
+                if index > begin and candidates[index - 1] == candidates[index]:
+                    continue
+
+                path.append(candidates[index])
+                dfs(index + 1, path, residue - candidates[index])
+                path.pop()
+
+        size = len(candidates)
+        if size == 0:
+            return []
+
+        candidates.sort()
+        res = []
+        dfs(0, [], target)
+        return res
+
 # @lc code=end
 

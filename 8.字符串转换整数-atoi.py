06523e44c7681f5e6bf59ae111f075b952cb8e2a
xk <xk@xk.com>
2020-07-09 21:25:00 +0800

    . 2020-07-09 21:25:00

--- 8.字符串转换整数-atoi.py
+++ 8.字符串转换整数-atoi.py
@@ -11,15 +11,20 @@ class Solution(object):
         :type str: str
         :rtype: int
         """
+        if not str:return 0
         syl = ["+","-","0","1","2",
-        "3","4","5","6","7","8","9"
+        "3","4","5","6","7","8","9"," "
         ]
         if str[0] not in syl:return 0
         result=""
         for i in str:
             if i in syl:
-                result +=i
-
+                if i != " ":
+                    result +=i
+            else:
+                break
+        print result
+        if result in ["+","-",""]:return 0
         if abs(int(result))>2**32:return -2147483648
         return int(result)
 # @lc code=end

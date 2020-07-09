56d84848f797febf41b89ff7c9435069a654bb66
xk <xk@xk.com>
2020-07-09 22:05:00 +0800

    . 2020-07-09 22:05:00

--- 8.字符串转换整数-atoi.py
+++ 8.字符串转换整数-atoi.py
@@ -5,6 +5,7 @@
 #
 
 # @lc code=start
+
 class Solution(object):
     def myAtoi(self, str):
         """
@@ -42,30 +43,44 @@ class Solution(object):
         #     return int(result)
         # except :
         #     return 0
-        i=0
-        n=len(str)
-        while i<n and str[i]==' ':
-            i=i+1
-        if n==0 or i==n:
-            return 0
-        flag=1
-        if str[i]=='-':
-            flag=-1
-        if str[i]=='+' or str[i]=='-':
-            i=i+1
-        INT_MAX=2**31-1
-        INT_MIN=-2**31
-        ans=0
-        while i<n and '0'<=str[i]<='9':
-            ans=ans*10+int(str[i])-int('0')
-            i+=1
-            if(ans-1>INT_MAX):
-                break
+        automaton = Automaton()
+        for c in str:
+            automaton.get(c)
+        return automaton.sign * automaton.ans
 
-        ans=ans*flag
-        if ans>INT_MAX:
-            return INT_MAX
-        return INT_MIN if ans<INT_MIN else ans
-    
+
+INT_MAX = 2 ** 31 - 1
+INT_MIN = -2 ** 31
+
+class Automaton:
+    def __init__(self):
+        self.state = 'start'
+        self.sign = 1
+        self.ans = 0
+        self.table = {
+            'start': ['start', 'signed', 'in_number', 'end'],
+            'signed': ['end', 'end', 'in_number', 'end'],
+            'in_number': ['end', 'end', 'in_number', 'end'],
+            'end': ['end', 'end', 'end', 'end'],
+        }
+        
+    def get_col(self, c):
+        if c.isspace():
+            return 0
+        if c == '+' or c == '-':
+            return 1
+        if c.isdigit():
+            return 2
+        return 3
+
+    def get(self, c):
+        self.state = self.table[self.state][self.get_col(c)]
+        if self.state == 'in_number':
+            self.ans = self.ans * 10 + int(c)
+            self.ans = min(self.ans, INT_MAX) if self.sign == 1 else min(self.ans, -INT_MIN)
+        elif self.state == 'signed':
+            self.sign = 1 if c == '+' else -1
+
+
 # @lc code=end
 

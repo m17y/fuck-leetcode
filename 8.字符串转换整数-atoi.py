6990332a8418b13d50f558ba3c691b719dffe53d
xk <xk@xk.com>
2020-07-09 21:10:00 +0800

    . 2020-07-09 21:10:00

---
+++ 8.字符串转换整数-atoi.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode.cn id=8 lang=python
+#
+# [8] 字符串转换整数 (atoi)
+#
+
+# @lc code=start
+class Solution(object):
+    def myAtoi(self, str):
+        """
+        :type str: str
+        :rtype: int
+        """
+# @lc code=end
+

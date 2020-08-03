cdad3c25f2815063d89b2f419990db8a4376c1eb
xk <xk@xk.com>
2020-08-03 21:25:00 +0800

    . 2020-08-03 21:25:00

--- coding/53.最大子序和.py
+++ coding/53.最大子序和.py
@@ -11,5 +11,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        l = len(nums)
+        if l==0: return 0
+        max = nums[0]
+        for i in range(1,l):
+            for j in rang(i+1,)
 # @lc code=end
 

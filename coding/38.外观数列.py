115c8467ad4be98c445faf74ba7f1697dd747975
xk <xk@xk.com>
2020-08-03 21:50:00 +0800

    . 2020-08-03 21:50:00

--- coding/38.外观数列.py
+++ coding/38.外观数列.py
@@ -11,6 +11,8 @@ class Solution(object):
         :type n: int
         :rtype: str
         """
-        
+        result = 1
+        for i in range(2,n+1):
+            result = i
         # @lc code=end
 

accd8ea3ff546acb6139d5fbe79ed8de45c5fb97
xk <xk@xk.com>
2020-08-04 20:35:00 +0800

    . 2020-08-04 20:35:00

---
+++ coding/61.旋转链表.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode.cn id=61 lang=python
+#
+# [61] 旋转链表
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def rotateRight(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
+# @lc code=end
+

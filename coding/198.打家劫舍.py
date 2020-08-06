655eb715b97ceb7e35837d2ef810b34eafae4dae
xk <xk@xk.com>
2020-08-06 21:45:00 +0800

    . 2020-08-06 21:45:00

--- coding/198.打家劫舍.py
+++ coding/198.打家劫舍.py
@@ -11,14 +11,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        if len(nums)<=0:return 0
         dp = [0 for i in nums]
         dp[0] = nums[0]
         l = len(nums)
         for i in range(1,l-1):
             print dp[i-1],nums[i+1],nums[i]
-            dp[i+1] =max(dp[i-1]+nums[i+1],nums[i])
+            dp[i+1] =max(dp[i-1]+nums[i+1],dp[i])
             dp[i]
         print dp
-        return dp[-1]
+        return max(dp)
 # @lc code=end
 

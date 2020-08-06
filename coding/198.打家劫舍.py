399feff0507fd61931c9c3bd19335a573d110333
xk <xk@xk.com>
2020-08-06 21:55:00 +0800

    . 2020-08-06 21:55:00

--- coding/198.打家劫舍.py
+++ coding/198.打家劫舍.py
@@ -11,17 +11,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if not nums:
-            return 0
-
-        size = len(nums)
-        if size == 1:
-            return nums[0]
+        if len(nums)<=0:return 0
         dp = [0 for i in nums]
         dp[0] = nums[0]
-        dp[1] = max(nums[0],nums[1])
-        for i in range(2,size):
-            dp[i] =max(dp[i-2]+nums[i],dp[i-1])
+        dp[1] = max()
+        l = len(nums)
+        for i in range(1,l-2):
+            print dp[i-1],nums[i+1],nums[i]
+            dp[i+1] =max(dp[i-1]+nums[i+1],dp[i])
+            dp[i]
         print dp
         return max(dp)
 # @lc code=end

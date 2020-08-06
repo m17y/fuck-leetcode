f44ce2cd19ef1914221ff079ebbf50cfa8231c6e
xk <xk@xk.com>
2020-08-06 21:35:00 +0800

    . 2020-08-06 21:35:00

--- coding/198.打家劫舍.py
+++ coding/198.打家劫舍.py
@@ -11,5 +11,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        dp = [0 for i in nums]
+        dp[0] = nums[0]
+        l = len(nums)
+        for i in range(1,l-1):
+            dp[i] =max(dp[i-1]+dp[i+1],dp[i])
+        print dp
+        return dp[-1]
 # @lc code=end
 

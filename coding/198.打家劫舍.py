8a005bb4793ce5f0493bb52c0034f977ab77111d
xk <xk@xk.com>
2020-08-06 21:30:00 +0800

    . 2020-08-06 21:30:00

---
+++ coding/198.打家劫舍.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode.cn id=198 lang=python
+#
+# [198] 打家劫舍
+#
+
+# @lc code=start
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+# @lc code=end
+

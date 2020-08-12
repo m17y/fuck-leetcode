be240cdd049d55dd51de4cd6c7611052601a6244
xk <xk@xk.com>
2020-08-12 19:55:00 +0800

    . 2020-08-12 19:55:00

--- coding/2.两数相加.py
+++ coding/2.两数相加.py
@@ -18,6 +18,7 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
+        # TODO xk
         origin=ListNode(0)
         a=0
         pre = origin
---
+++ coding/29.两数相除.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode.cn id=29 lang=python
+#
+# [29] 两数相除
+#
+
+# @lc code=start
+class Solution(object):
+    def divide(self, dividend, divisor):
+        """
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        """
+# @lc code=end
+

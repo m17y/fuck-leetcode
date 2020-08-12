d1042855ded3fc71edc2c68dd22a2f2820a1ed2a
xk <xk@xk.com>
2020-08-12 20:00:01 +0800

    . 2020-08-12 20:00:01

--- coding/29.两数相除.py
+++ coding/29.两数相除.py
@@ -6,11 +6,25 @@
 
 # @lc code=start
 class Solution(object):
+    def xx(self,num,dividend, divisor):
+        divisor+divisor
+        if (dividend-divisor+divisor)>divisor:
+        return num+num,dividend,divisor
+
     def divide(self, dividend, divisor):
         """
         :type dividend: int
         :type divisor: int
         :rtype: int
         """
+        z = 1
+        r = 0
+        while dividend>divisor:
+            dividend = dividend-divisor
+            r=r+z
+            z,dividend,divisor = xx(z,dividend,divisor)
+
+        return r
+
 # @lc code=end
 

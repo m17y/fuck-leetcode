540e93bab68d8d8283849fc87518d6bb51f86168
xk <xk@xk.com>
2020-08-13 21:50:00 +0800

    . 2020-08-13 21:50:00

---
+++ coding/39.组合总和.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode.cn id=39 lang=python
+#
+# [39] 组合总和
+#
+
+# @lc code=start
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+# @lc code=end
+

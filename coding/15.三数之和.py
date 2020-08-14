fc158ca5d43920baa7affb9e8e2f7ff31fe1f6e5
xk <xk@xk.com>
2020-08-14 21:15:00 +0800

    . 2020-08-14 21:15:00

---
+++ coding/15.三数之和.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode.cn id=15 lang=python
+#
+# [15] 三数之和
+#
+
+# @lc code=start
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        0 1 2 -1 -1 -4
+# @lc code=end
+

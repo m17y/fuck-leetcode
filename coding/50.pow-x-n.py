b077375f072f3b04327995dce44d487fdd230bf5
xk <xk@xk.com>
2020-09-15 21:15:00 +0800

    . 2020-09-15 21:15:00

--- coding/50.pow-x-n.py
+++ coding/50.pow-x-n.py
@@ -12,7 +12,17 @@ class Solution(object):
         :type n: int
         :rtype: float
         """
-        def quickmul(N):
+        def quickMul(N):
             ans = 1.0
+            x_contribute = x
+            while N > 0:
+                if N % 2 == 1:
+                    ans *= x_contribute
+                x_contribute *= x_contribute
+                N //= 2
+            return ans
+        
+        return quickMul(n) if n >= 0 else 1.0 / quickMul(-n)
+
 # @lc code=end
 
